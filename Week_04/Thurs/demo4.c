ad914fa6417bf116e64dc900c14564ddf51c2fae
Donna Calhoun <donnacalhoun@boisestate.edu>
2018-02-06 08:25:51 -0700

    (Week_04/Thurs) Using reduce

---
+++ Week_04/Thurs/demo4.c
@@ -0,0 +1,88 @@
+#include "demo4.h"
+#include "demo_util.h"
+
+#include <stdio.h>
+#include <mpi.h>
+
+void main(int argc, char** argv)
+{
+    /* Data arrays */
+    double *x;
+    double s, total_sum;
+    double mean;
+
+    /* MPI variables */
+    int my_rank, nprocs;
+    int tag = 0;
+    int count;
+    int source;
+    MPI_Status status;
+
+    /* Other local variables */
+    int i, p;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    int n_global = pow2(10);
+    int n_local = n_global/nprocs;
+
+    if (my_rank == 0)
+    {
+        random_array(n_global,&x);  /* Used for both sending and receiving */
+        int p;
+        for(p = 1; p < nprocs; p++)
+        {
+            int dest = p;
+            MPI_Send((void*) &x[p*n_local],n_local,MPI_DOUBLE,dest,tag,MPI_COMM_WORLD);
+        }
+    }
+    else
+    {
+        source = 0;
+        empty_array(n_local,&x);
+        MPI_Recv((void*) x,n_local,MPI_DOUBLE,source,tag,MPI_COMM_WORLD,&status);
+        MPI_Get_count(&status,MPI_DOUBLE,&count);
+        if (count < n_local)
+        {
+            printf("Something went wrong\n");
+            printf("n_local = %d\n",n_local);
+            printf("count = %d\n",count);
+            exit(0);
+        }
+    }
+
+
+    source = 0;
+    s = sum_array(n_local,x);   
+    MPI_Reduce(&s,&total_sum,1,MPI_DOUBLE,MPI_SUM,source,MPI_COMM_WORLD);    
+    mean = total_sum/n_global;
+
+    printf("Processor %d : The mean is  %.16f\n",my_rank,mean);        
+
+    if (my_rank == 0)
+    {
+        double mean_true;
+        mean_true = sum_array(n_global,x)/n_global;
+        printf("Processor %d : True mean is %.16f\n",my_rank,mean_true);
+    }
+
+    /* Fill in details for broadcasting to all processors, computing 
+    sums needed for STD, and then reducing results */
+
+#if 0
+    source = 0;
+    MPI_Bcast(&mean,1,MPI_DOUBLE,source,MPI_COMM_WORLD);
+
+    printf("The mean is %16.8f (from rank %d)\n",mean,my_rank);
+#endif    
+
+
+    delete_array(&x);
+
+    MPI_Finalize();
+
+}
---
+++ Week_04/Thurs/demo4.h
@@ -0,0 +1,15 @@
+#ifndef DEMO4_H
+#define DEMO4_H
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/* Nothing yet */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
