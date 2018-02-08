223d4d9e0a54d80763c06b08c7b390098c94181f
Donna Calhoun <donnacalhoun@boisestate.edu>
2018-02-08 08:31:37 -0700

    (Week_05/Thurs) Adding demo7 for trapezoidal rule

---
+++ Week_05/Thurs/demo7.c
@@ -0,0 +1,72 @@
+#include "demo7.h"
+#include <demo_util.h>
+
+#include <mpi.h>
+#include <math.h>
+
+#define PI 3.141592653589793116
+
+/* Integrate this over [0,1] */
+double f(double x)
+{
+    double fx;
+
+    fx = exp(x)*pow(sin(2*PI*x),2);
+    return fx;
+}
+
+/* Indefinite integral goes here */
+double I_exact(double x)
+{
+    /* Use Wolframe Alpha to code indefinite integral */
+    return 0;
+}
+
+void main(int argc, char** argv)
+{
+    /* Data arrays */
+    double *x;
+    double s, total_sum;
+    double mean;
+    int n_global;
+
+    /* MPI variables */
+    int my_rank, nprocs;
+    int tag = 0;
+    int count;
+    int source;
+    MPI_Status status;
+
+    /* Other local variables */
+    int i, p;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    set_rank(my_rank);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    if (nprocs < 2)
+    {
+        print_global("demo7 : Must have at least 2 processors.\n");
+        exit(0);
+    }
+
+    if (my_rank == 0)
+    {        
+        int p0;
+        read_int(argc,argv, "-p",&p0);
+        n_global = pow2(p0);     /* Number of sub-intervals used for integration */
+
+        /* Your Node 0 work goes here */
+
+    }
+    else
+    {
+        /* Your node P>0 goes here */
+    }
+
+    MPI_Finalize();
+
+}
