480321be5499ab2d5b4aedd3fc32f4b842936370
Donna Calhoun <donnacalhoun@boisestate.edu>
2018-02-26 18:03:58 -0700

    (Week 8/Tues) Added hmwk3_demo file

---
+++ Week_08/Tues/hmwk3_demo.c
@@ -0,0 +1,146 @@
+#include "hmwk3.h"
+#include <demo_util.h>
+
+#include <mpi.h>
+#include <math.h>
+
+#define PI 3.14159265358979323846264338327
+
+static
+void fill_ghost(double *u, int m, int my_rank);
+
+double utrue(double x)
+{
+    double u;
+    double pi2;
+    pi2 = 2*PI;
+    u = cos(pi2*x);
+    return u;
+}
+
+double rhs(double x)
+{
+    double fx;
+    double pi2;
+    pi2 = 2*PI;
+    fx = -(pi2)*(pi2)*cos(pi2*x);
+    return fx;
+}
+
+
+void main(int argc, char** argv)
+{
+    /* Data arrays */
+    double a,b;
+    int n_global;
+    double *x, *F, *B;
+    double range[2];
+
+    /* Iterative variables */
+    double tol;
+    int kmax,i,j,k;
+
+    /* MPI variables */
+    int my_rank, nprocs;
+
+    MPI_Init(&argc, &argv);
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    set_rank(my_rank);  /* Used in printing */
+    read_loglevel(argc,argv);
+
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    if (my_rank == 0)
+    {        
+        int m,err,loglevel;
+        read_int(argc,argv, "-m", &m, &err);
+        if (err > 0)
+        {
+            print_global("Command line argument '-m' not found\n");
+            exit(0);
+        }        
+        read_int(argc,argv, "--kmax", &kmax, &err);
+        if (err > 0)
+        {
+            print_global("Command line argument '--kmax' not found\n");
+            exit(0);
+        }
+        read_double(argc,argv, "--tol", &tol, &err);
+        if (err > 0)
+        {
+            print_global("Command line argument '--tol' not found\n");
+            exit(0);
+        }
+
+        n_global = pow2(m);     /* Number of sub-intervals used for integration */
+
+        /* Hardwire domain values values */
+        a = 0;
+        b = 1;  
+    }
+
+    /* ---------------------------------------------------------------
+       Broadcast global information : kmax, tol, n_global
+         -- compute range, h, and number of intervals for each
+            processor
+    --------------------------------------------------------------- */
+
+    /* TODO : .... */
+
+    /* ---------------------------------------------------------------
+       Set up right hand side and any inhomogenous boundary conditions
+    --------------------------------------------------------------- */
+
+    /* TODO : .... */
+
+    /* ----------------------------------------------------------------
+       Set up arrays and other vars needed for iterative method
+    ---------------------------------------------------------------- */
+
+    /* TODO : .... */
+
+
+    /* ----------------------------------------------------------------
+       Start iterations
+    ---------------------------------------------------------------- */
+
+    /* TODO : .... */
+
+    /* ----------------------------------------------------------------
+       Calculate error and report results
+    ---------------------------------------------------------------- */
+
+    /* TODO : .... */
+
+    /* ----------------------------------------------------------------
+       Clean up
+    ---------------------------------------------------------------- */
+    delete_array(&B);
+    delete_array(&F);
+    delete_array(&x);
+    delete_array(&uk);
+    delete_array(&rk);
+    delete_array(&zk);
+
+    MPI_Finalize();
+}
+
+
+/* ----------------------------------------------------------------
+   Fill ghost cell values
+---------------------------------------------------------------- */
+static
+void fill_ghost(double *u, int m, int my_rank)
+{
+    int tag, source, dest;
+    int nprocs;
+    MPI_Status status;
+
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
+
+    /* Fill ghost cells in u[0], u[m+2] */
+
+    /* ...... */
+
+}
